e83a41c6e0fdffe664396da543e204513c287a44
Ricardo  Perdomo <ricardoperdomo@Ricardos-MacBook-Pro.local>
2022-11-02 11:39:32 -0400

    Aplicacion con debouncer

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: unnecessary_this
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas/helpers/debouncer.dart';
 import 'package:peliculas/models/models.dart';
 import 'package:peliculas/models/search_movie_response.dart';
 
@@ -16,6 +19,14 @@ class MoviesProvider extends ChangeNotifier {
   Map<int, List<Cast>> moviesCast = {};
 
   int _popularPage = 0;
+
+  final debouncer = Debouncer(
+    duration: Duration(milliseconds: 500),
+  );
+  final StreamController<List<Movie>> _suggestionStreamController =
+      new StreamController.broadcast();
+  Stream<List<Movie>> get suggestionStream =>
+      this._suggestionStreamController.stream;
 
   MoviesProvider() {
     this.getOnDisplayMovies();
@@ -59,5 +70,17 @@ class MoviesProvider extends ChangeNotifier {
     final searchResponse = SearchMovieResponse.fromJson(response.body);
 
     return searchResponse.results;
+  }
+
+  void getSuggestionsByQuery(String searchTerm) {
+    debouncer.value = '';
+    debouncer.onValue = (value) async {
+      final results = await this.searchMovies(value);
+      this._suggestionStreamController.add(results);
+    };
+    final timer = Timer.periodic(Duration(milliseconds: 300), (_) {
+      debouncer.value = searchTerm;
+    });
+    Future.delayed(Duration(milliseconds: 301)).then((_) => timer.cancel());
   }
 }
